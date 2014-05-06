31c6939cdcea0de2a60bd389548e7e68fdb0ae54
Lyzi Diamond <lyzidiamond@gmail.com>
2014-05-06 16:48:46 -0700

    Spike on adding amenities to popups

--- app/assets/javascripts/parks.js.jsx
+++ app/assets/javascripts/parks.js.jsx
@@ -163,6 +163,63 @@ $(function() {
   
   var map;
 
+  var amenities = {
+    "AMPHITHEAT": "Amphitheater",
+    "BASKETBALL": "Basketball Court",
+    "HARDCOURT": "Hardcourt",
+    "BOAT": "Boats",
+    "DAY_CAMP": "Day Camp",
+    "DISC_GOLF": "Disc Golf Course",
+    "DOG_PARKS": "Dog Park",
+    "EQUESTRIAN": "Horse Facilities",
+    "FISHING": "Fishing",
+    "GOLF": "Golf Course",
+    "GYMNASIUM": "Gymnasium",
+    "HORSESHOES": "Horseshoes",
+    "LAKE_POND": "Lake and/or Pond",
+    "OPEN_FIELD": "Open Field and/or Passive Wooded Area",
+    "PAVED_TRAI": "Paved Trails",
+    "PLAYGROUND": "Playground",
+    "SPECIAL_EV": "Special Events Space",
+    "SWIMMING": "Swimming",
+    "TENNIS_COU": "Tennis Courts",
+    "VOLLEYBALL": "Volleyball Courts",
+    "WEIGHT_ROO": "Weight Rooms",
+    "BALLFIELD": "Baseball and/or Softball Fields",
+    "CC_YOUTH": "Community Centers for Seniors",
+    "CC_SENIOR": "Community Centers for Adults",
+    "GARDENS": "Community Gardens",
+    "FOOTBALLS": "Football and Soccer Fields",
+    "NAT_PRGMS": "Nature Programs",
+    "NGHBRBLDGS": "Neighborhood Buildings",
+    "PICNICTBLS": "Picnic Tables",
+    "GRILLS": "Grills",
+    "UNPVD_TRLS": "Unpaved Trails",
+    "SHELTERS_": "Shelters"
+  };
+
+  function popup(feature) {
+    var park = feature.properties;
+    var popupText = '<strong>' + park.PARK_NAME + '</strong><br><em>' + park.ADDRESS_1 + '</em>';
+    for (var amenity in park) {
+      if (park.hasOwnProperty(amenity)) {
+        if ((park[amenity] == "Yes" || park[amenity] > 0) && amenities[amenity]) {
+          console.log(amenity);
+          popupText += "<br>- " + amenities[amenity];
+        };
+      };
+    };
+    return popupText;
+  };
+
+  function onEachFeature(feature, layer) {
+    var park = feature.properties;
+    layer.on('click', function(e) {
+      map.fitBounds(e.target.getBounds());
+    });
+    layer.bindPopup(popup(feature));
+  };
+
   map = L.map("map", {
     zoom: 11,
     center: [38.042,-84.515],
@@ -182,12 +239,7 @@ $(function() {
         fillOpacity: 0.6
       };
     },
-    onEachFeature: function(feature, layer) {
-      layer.on('click', function(e) {
-        map.fitBounds(e.target.getBounds());
-      });
-      layer.bindPopup("This is " + feature.properties.PARK_NAME + "!");
-    }
+    onEachFeature: onEachFeature
   })
   .addTo(map);
 
